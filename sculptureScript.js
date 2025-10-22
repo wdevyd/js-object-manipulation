10e342de32aa2e607a6221670cf6cf0c2e5a1437
david <dwu2343@gmail.com>
2025-10-21 17:44:10 -0700

    [ADD] sculptureScript.js : completed code

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,10 +1,23 @@
 const sculptureList = require('./data.js'); // import data.js
 
-const element = sculptureList[0] // <---- comment out this line in your solution!
+//const element = sculptureList[0] // <---- comment out this line in your solution!
 
 
 // this following snippet is just to show how to iterate an object's keys
 // comment this out in your solution!
-for (const key in element){
-    console.log(`${key}: ${element[key].length}`)
+//for (const key in element){
+//    console.log(`${key}: ${element[key].length}`)
+//}
+
+
+var sculptureListLengths = {};
+
+Object.assign(sculptureListLengths, sculptureList);
+for (const key in sculptureListLengths) {
+    const curr = sculptureListLengths[key];
+    for (currKey in curr){
+        curr[currKey] = curr[currKey].length;
+    }
 }
+
+console.log(sculptureListLengths);
